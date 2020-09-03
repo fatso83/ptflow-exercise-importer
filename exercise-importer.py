21ae046f7c5927f21aeeb953be99126cd72bcec3
Carl-Erik Kopseng <carlerik@gmail.com>
2020-09-03 07:24:45 +0200

    Refactor logic into manageable chunks

--- exercise-importer.py
+++ exercise-importer.py
@@ -28,7 +28,10 @@ from __future__ import print_function
 import pickle
 import glob
 import logging
-import os.path
+import os
+import sys
+import argparse
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -45,19 +48,97 @@ except Exception as e:
 ch = logging.StreamHandler()
 logger.addHandler(ch)
 
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+parser = argparse.ArgumentParser()
+parser.add_argument(
+        "--image-dir", type=str, required=True,
+        help="A directory containing image files that are named using a specific naming scheme: {id}-.*.png")
+parser.add_argument(
+        "--sheets-id", type=str, 
+        help="The id of the Google Sheet containing exercise data (i.e. '18_LuqnjAmVzAL6zQzJKjSWqGgPMLgYx0I_k3wV2I2xg'")
+parser.add_argument(
+        "--bookkeeping-id", type=str, 
+        help="A worksheet with this name will be created in the spreadsheet to keep tabs on what data has been uploaded")
+parser.add_argument(
+        "--session", type=str, 
+        help="A valid session token taken from a browser to use when " 
+        + "performing REST calls to the PTFLOW server. "
+        + "Can also be set using the environment variable PTFLOW_SESSION.")
+parser.add_argument(
+        "--server", type=str, 
+        help="The server url, including an optional port number (i.e. https://myserver.dev:443). "
+        + "Can also be set using the environment variable PTFLOW_SERVER.")
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = "18_LuqnjAmVzAL6zQzJKjSWqGgPMLgYx0I_k3wV2I2xg"
 RANGE_NAME = "Øvelser pri 1!A5:J"
-IMAGE_DIRECTORY="/Users/carlerik/ptflow-exercises/PACK/"
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+
+    parsed = parser.parse_args()
+    try:
+        session = parsed.session or os.environ["PTFLOW_SESSION"]
+        server = parsed.server or os.environ["PTFLOW_SERVER"]
+    except KeyError as e:
+        print("Server and session are required")
+        sys.exit(1)
+
+    image_dir = parsed.image_dir
+    sheets_id = parsed.sheets_id or SPREADSHEET_ID
+
+    print("session: %s\nserver: %s"%(session,server)) 
+
+    values = get_spreadsheet_values(sheets_id, RANGE_NAME)
+    # A stub for debugging
+    # values = [["0003","foo","something"]]
+
+    if not values:
+        logger.error("No data found in spreadsheet.")
+        sys.exit(1)
+    
+    logger.info("Got %s results from Google Sheets", len(values))
+
+    for row in values:
+        exercise = Exercise(row)
+
+        images = get_images(image_dir, exercise.id)
+        exercise.images.extend(images)
+
+        createExercise(exercise, server, session, parsed.bookkeeping_id)
+
+
+def createExercise(exercise,server, session, bookkeeping_id):
+
+    # check if already uploaded - return early if so
+
+    # upload images, get ids
+
+    # upload exercise with image ids
+
+    # log data to bookkeeping
+
+def get_images(image_dir, exercise_id):
+    # All these image paths assume the image dir is the subdir ./PACK
+    glob_string = image_dir + exercise_id + "*.png"
+    single_file_glob_string = image_dir + "SINGLE-STEP/" + exercise_id + "*SINGLE-STEP.png"
+    image_list = glob.glob(glob_string)
+    single_file = glob.glob(single_file_glob_string)
+
+    assert not (len(single_file) > 0 and len(image_list) > 0)
+
+    if image_list:
+        image_list.sort()
+        return image_list
+    elif single_file:
+        logger.debug("id=%s. Single image: %s" % (exercise_id, single_file[0]))
+        return single_file
+    else:
+        raise Exception("No image found for id " + exercise_id) 
+  
+
+def get_spreadsheet_values(sheets_id, spreadsheet_range):
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
     creds = None
     # The file token.pickle stores the user"s access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -82,40 +163,39 @@ def main():
     # Call the Sheets API
     logger.debug("Calling sheets API")
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                range=RANGE_NAME).execute()
-    values = result.get("values", [])
-    # A stub for debugging
-    # values = [("0003","foo","something")]
-    
-    logger.info("Got %s results from Google Sheets", len(values))
+    result = sheet.values().get(spreadsheetId=sheets_id,
+                                range=spreadsheet_range).execute()
 
-    if not values:
-        logger.error("No data found.")
-    else:
-        for row in values:
-            id = row[0]
-            glob_string = IMAGE_DIRECTORY + id + "*.png"
-            single_file_glob_string = IMAGE_DIRECTORY + "SINGLE-STEP/" + id + "*SINGLE-STEP.png"
-            image_list = glob.glob(glob_string)
-            single_file = glob.glob(single_file_glob_string)
+    return result.get("values", [])
 
-            assert not (len(single_file) > 0 and len(image_list) > 0)
-            if image_list:
-                image_list.sort()
-                image1 = image_list[0]
+class Exercise:
+    def __init__(self, row):
+        required_length = 9+1
+        list_length = len(row)
+        missing_vals = required_length-list_length
 
-                logger.debug("%s, %s. Multiple images. Showing first: %s" % (row[0], row[2], image1))
-                if len(image_list) > 2:
-                    logger.warning("More than two images for id \"%s\". Skipping exercise creation", id)
-                    continue
-            elif single_file:
-                logger.debug("%s, %s. Single image: %s" % (row[0], row[2], single_file[0]))
-            else:
-                raise Exception("No image found for id " + id) 
+        if missing_vals > 0: 
+            row.extend(['']*missing_vals)
+            self.extended = True
+        else:
+            self.extended = False
 
-# def createExercises():
-    # todo
+        self.id = row[0]
+        self.priority = row[1]
+        self.name = row[2]
+        self.description1 = row[3]
+        self.description2 = row[4]
+        self.category = row[5]
+        self.equipment = row[6]
+        self.bodypart = row[7]
+        self.target = row[8]
+        self.synergist = row[9]
+
+        self.images = []
+
+    def __str__(self):
+        args = (self.id, self.name, self.equipment, self.bodypart, self.target)
+        return "Exercise{id=%s, name=%s, equipment=%s, bodypart=%s, target=%s}"%args
 
 if __name__ == "__main__":
     main()
