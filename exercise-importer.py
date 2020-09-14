ad716cd73117d892673efcc32940676ce5057d17
Carl-Erik Kopseng <carlerik@gmail.com>
2020-09-14 23:07:25 +0200

    Use the focus for subtype value

--- exercise-importer.py
+++ exercise-importer.py
@@ -480,15 +480,20 @@ class Exercise:
         id = row[0]
         name = row[4]
         type = convert_type(row[5])
-        subtype = row[6]
-        # Drop setting the focus type, due to crap data in spreadsheet
-        # Ref https://pt-flow.slack.com/archives/GQ2QX4HNJ/p1600112328050900
-        # focus_prim = convert_focus(row[9])
-        # focus_sec = convert_focus(row[10])
+
+        focus_prim = convert_focus(row[9])
+        focus_sec = convert_focus(row[10])
+
+        # subtype = row[6]
+        # Just use the focus prim value, ref Slack talk
+        subtype = focus_prim if type == 'STRENGTH' and focus_prim in SUBTYPES
+
         description = row[11]
         notes = ''
 
-        return Exercise(id, name, description, type, subtype, focus_prim = '', focus_sec = '')
+        # Drop setting the focus type, due to crap data in spreadsheet
+        # Ref https://pt-flow.slack.com/archives/GQ2QX4HNJ/p1600112328050900
+        return Exercise(id, name, description, type, subtype, '', '')
 
     def __init__(self, id, name, description, type, subtype, focus_prim, focus_sec):
         self.id = id # this is the simple id from the spreadsheet, not the UUID from server
